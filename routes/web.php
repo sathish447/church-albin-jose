7cb1a0b7e669074a376bf8b35d396d2931c32e54
sathish447 <sathishprabu447@gmail.com>
2024-04-09 17:00:10 +0530

    login and dashboard routes update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+	Route::get(conf('backend_url'), [LoginController::class, 'showLoginForm'])->name('login');
+	Route::post(conf('backend_url'), [LoginController::class, 'login'])->name('loginPost');
+	Route::post(conf('backend_url').'/logut', [LoginController::class, 'logout'])->name('logout');
+
+	// Admin Routes
+
+	Route::group(['prefix' => conf("backend_url"), 'namespace' => 'App\Http\Controllers\Admin', 'middleware' => ['web', 'auth']], function () {
+
+        // Dashboard
+        Route::get('dashboard', [
+            'as' => 'admin.dashboard',
+            'uses' => 'DashboardController@index',
+            // 'middleware' => ['checkPrivilege:dev;dashboard'],
+        ]);
+
+    });
+
