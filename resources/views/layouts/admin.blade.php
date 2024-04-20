4edaf563482d7efb07126f7c254ca3bd5fe34bf0
sathish447 <sathishprabu447@gmail.com>
2024-04-20 16:40:41 +0530

    admin page setup yeald nav bar side bar al set

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,27 +1,34 @@
+@php use Illuminate\Support\Facades\Auth; @endphp
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>@yield('title')</title>
-  <!-- Include your CSS links here -->
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
-  <!-- DataTables -->
-  <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
 
-  @yield('head-script')
-  @stack('head-script')
-  @yield('style')
-</head>                                     
-<body class="hold-transition sidebar-mini">
-  <div class="wrapper">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>
+        @yield('page_title', conf('name'))
+    </title>
+    <link rel="icon" href="{{ btheme() }}/favicon.png?{{ ver() }}" type="image/png" sizes="220x220">
+    <link rel="stylesheet" href="{{ btheme() }}/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="{{ btheme() }}/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="{{ btheme() }}/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <link rel="stylesheet" href="{{ btheme() }}/plugins/toastr/toastr.min.css">
+    <link rel="stylesheet" href="{{ btheme() }}/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+    <link rel="stylesheet" href="{{ btheme() }}/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="{{ btheme() }}/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="{{ btheme() }}/plugins/datatables-rowreorder/css/rowReorder.bootstrap4.min.css">
+    <link rel="stylesheet" href="{{ btheme() }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="{{ btheme() }}/css/custom.css">
+
+    @yield('head-script')
+    @stack('head-script')
+    @yield('style')
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
 
     <?php
       if (!isset($active_sub)) $active_sub = '*********';
@@ -29,32 +36,56 @@
     ?>
 
       <!-- Preloader -->
-  <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-  </div>
-
-    <!-- Navbar -->
-    {{-- @include('layouts.navbar') --}}
-    <!-- Sidebar -->
-    @include('layouts.sidebar')
-    <!-- Content Wrapper -->
-    <div class="content-wrapper">
-      @yield('content')
+    <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="" src="{{ btheme() }}/images/logo.png?{{ ver() }}" alt="{{ conf('name') }}" width="200">
     </div>
-    <!-- Footer -->
-    @include('layouts.footer')
+
+    @include('layouts.navbar')
+
+    @include('layouts.sidebar')
 
 
-  </div>
+    <div class="content-wrapper">
+        <!-- breadcrums -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">
+                            @yield('page_title')
+                        </h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            @yield('breadcrumb')
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-      @if ($errors->any())
+        <section class="content">
+            <div class="container-fluid">
+                @yield('content')
+            </div>
+        </section>
+    </div>
+       @include('layouts.footer')
+</div>
+
+@if ($errors->any())
     <div style="display:none" id="error_list">{!! implode('<br/>', $errors->all()) !!}</div>
 @endif
+
+<div class="loading" id="loaderModal" style="display: none;">
+    <div class="spinner">
+        <div class="double-bounce1"></div>
+        <div class="double-bounce2"></div>
+    </div>
+</div>
 
   <!-- Scripts -->
   @include('layouts.scripts')
 
   @yield('scripts')
   @stack('scripts')
-</body>
-</html>
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,24 +1,24 @@
 <!-- jQuery -->
-<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ btheme() }}/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ btheme() }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- DataTables & Plugins -->
-<script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('plugins/jszip/jszip.min.js') }}"></script>
-<script src="{{ asset('plugins/pdfmake/pdfmake.min.js') }}"></script>
-<script src="{{ asset('plugins/pdfmake/vfs_fonts.js') }}"></script>
-<script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+<script src="{{ btheme() }}/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="{{ btheme() }}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{ btheme() }}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="{{ btheme() }}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="{{ btheme() }}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="{{ btheme() }}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="{{ btheme() }}/plugins/jszip/jszip.min.js"></script>
+<script src="{{ btheme() }}/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="{{ btheme() }}/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="{{ btheme() }}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="{{ btheme() }}/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="{{ btheme() }}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
+<script src="{{ btheme() }}/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{ asset('dist/js/demo.js') }}"></script>
+<script src="{{ btheme() }}/dist/js/demo.js"></script>
 <!-- Page specific script -->
 
 <script type="text/javascript">
