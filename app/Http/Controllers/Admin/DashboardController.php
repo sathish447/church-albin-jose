7a920795bf0726a95a1390913b369cf378b82071
sathish447 <sathishprabu447@gmail.com>
2024-04-25 12:41:55 +0530

    dashboard correction

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -9,6 +9,6 @@ class DashboardController extends Controller
 {
     public function index()
     {
-    	return view('dashboard.admin-index');
+    	return view('dashboard.index');
     }
 }
---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.admin')
+
+@section('page_title')
+    Dashboard
+@endsection
+
+@section('breadcrumb')
+@endsection
+
+@section('content')
+    <div class="row">
+
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>1000<sup style="font-size: 20px">₹</sup></h3>
+
+                    <p>Total Amount Today</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+
+    </div>
+@endsection
+
+@section('style')
+@endsection
+
+@section('head-script')
+@endsection
+
+@section('script')
+@endsection
